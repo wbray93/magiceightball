150835591de3bc8fcc05e182ecb8b52434cad40b
wbray93 <wbray053@gmail.com>
2019-01-26 16:12:49 -0800

    Added cleaner logic and tried to improve the function

--- index.js
+++ index.js
@@ -1,43 +1,79 @@
-let userName = '';
+let userName = "";
 
-userName === 'Jane' ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+userName === "Jane"
+  ? console.log(`Hello, ${userName}!`)
+  : console.log("Hello!");
 
-let userQuestion = 'What is my future career';
+let userQuestion = "What is my future career";
+// How would I make the pushed array value the question
+
 
 console.log(`${userName}${userQuestion}?`);
 
-let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
 
-let eightBall = '';
-
-switch (randomNumber) {
-  case 0:
-    eightBall = 'Programmer';
-    	break;
-  case 1:
-    eightBall = 'Drop Out';
-    	break;
-  case 2:
-    eightBall = 'Retire at 25';
-    	break;
-  case 3:
-    eightBall = 'Dental Hygenist';
-    	break;
-  case 4:
-    eightBall = 'Go home and re-think your life!';
-    	break;
-  case 5:
-    eightBall = 'Uncertain';
-    	break;
-  case 6:
-    eightBall = 'Ask again';
-    	break;
-  case 7:
-    eightBall = 'What your mom tells you!';
-    	break;
-  case 8:
-    eightBall = 'I don\'t get the question';
-    	break;
-};
-
-console.log(`The eight ball answered: ${eightBall}`);
+// switch (randomNumber) {
+//   case 0:
+//     eightBall = 'Programmer';
+//     	break;
+//   case 1:
+//     eightBall = 'Drop Out';
+//     	break;
+//   case 2:
+//     eightBall = 'Retire at 25';
+//     	break;
+//   case 3:
+//     eightBall = 'Dental Hygenist';
+//     	break;
+//   case 4:
+//     eightBall = 'Go home and re-think your life!';
+//     	break;
+//   case 5:
+//     eightBall = 'Uncertain';
+//     	break;
+//   case 6:
+//     eightBall = 'Ask again';
+//     	break;
+//   case 7:
+//     eightBall = 'What your mom tells you!';
+//     	break;
+//   case 8:
+//     eightBall = 'I don\'t get the question';
+//     	break;
+// };
+function theEightBall() {
+  document.getElementById("#eightball");
+  {
+    let randomNumber = Math.floor(Math.random() * 8);
+    switch (randomNumber) {
+      case 0:
+        eightBall = "Programmer";
+        break;
+      case 1:
+        eightBall = "Drop Out";
+        break;
+      case 2:
+        eightBall = "Retire at 25";
+        break;
+      case 3:
+        eightBall = "Dental Hygenist";
+        break;
+      case 4:
+        eightBall = "Go home and re-think your life!";
+        break;
+      case 5:
+        eightBall = "Uncertain";
+        break;
+      case 6:
+        eightBall = "Ask again";
+        break;
+      case 7:
+        eightBall = "What your mom tells you!";
+        break;
+      case 8:
+        eightBall = "I don't get the question";
+        break;
+    }
+    console.log(`The eight ball answered: ${eightBall}`);
+  }
+}
